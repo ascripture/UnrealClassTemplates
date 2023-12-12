068703bfe1f8b44c8cabf26be98c6aefbb17cd6c
Philipp Schladitz <7261221+ScriptedAdventure@users.noreply.github.com>
2023-12-12 19:34:15 +0100

    Add ActorComponent Template

---
+++ ActorComponent.cpp
@@ -0,0 +1,26 @@
+// MyActorComponent.cpp
+#include "MyActorComponent.h"
+
+// Sets default values for this component's properties
+UMyActorComponent::UMyActorComponent()
+{
+    // Set this component to be initialized when the game starts, and to be ticked every frame.
+    // You can turn these features off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+// Called when the game starts
+void UMyActorComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Custom initialization
+}
+
+// Called every frame
+void UMyActorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    // Add custom logic for each frame here
+}
