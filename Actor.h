b554d6bbb577ef0171247dcf8ece454120982d2c
ScriptedAdventure <7261221+ScriptedAdventure@users.noreply.github.com>
2023-12-14 13:06:08 +0100

    Create Actor.h

---
+++ Actor.h
@@ -0,0 +1,27 @@
+// MyActor.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class MYGAME_API AMyActor : public AActor
+{
+    GENERATED_BODY()
+    
+public:    
+    // Sets default values for this actor's properties
+    AMyActor();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:    
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Actor properties and methods go here
+};
